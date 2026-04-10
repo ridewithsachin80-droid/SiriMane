f8ab2398545e9f539ff93cef3265c3294fa52b66
ridewithsachin80-droid <ridewithsachin80@gmail.com>
2026-04-10 16:50:23 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,19 @@ app.use('/api', require('./routes/index'));
 app.get('/health', (req, res) => res.json({ status: 'ok', time: new Date().toISOString() }));
 
 app.use(express.static(path.join(__dirname, '../frontend/public')));
+
+// Root
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../frontend/public/home.html')));
+
+// Guest portal - old Firebase URL + new short URLs
+app.get('/siri-mane-guest-portal', (req, res) => res.sendFile(path.join(__dirname, '../frontend/public/guest.html')));
+app.get('/guest', (req, res) => res.sendFile(path.join(__dirname, '../frontend/public/guest.html')));
+
+// Management - old Firebase URL + new short URLs
+app.get('/siri-mane-management', (req, res) => res.sendFile(path.join(__dirname, '../frontend/public/index.html')));
+app.get('/management', (req, res) => res.sendFile(path.join(__dirname, '../frontend/public/index.html')));
+
+// Catch-all
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/public/home.html')));
 
 app.use((err, req, res, next) => {
